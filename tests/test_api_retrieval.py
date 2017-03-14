bcbfd9a166353fb6f1d43e1c3a2478820de050cf
Ryeland Gongora <rsgmon@gmail.com>
2017-03-14 15:49:46 -0700

    ghgh

--- tests/test_api_retrieval.py
+++ tests/test_api_retrieval.py
@@ -11,9 +11,9 @@ Tests for `api_retrieval` module.
 
 import unittest
 from api_retrieval.setup_api_parameters import *
-# from tests.test_data.test_data import test_config, api_config, single_api_config, api_config_with_key_route
+from tests.test_data.test_data import test_config, api_config, single_api_config, api_config_with_key_route
 # # from api_retrieval.http_request import HTTPRequestHandler
-# import api_retrieval.config
+import api_retrieval.config
 #
 #
 class TestApi_retrieval(unittest.TestCase):
