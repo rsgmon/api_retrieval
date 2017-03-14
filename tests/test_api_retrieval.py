4c9afefa6b895fbc5345789df0186164b6b0c59b
Ryeland Gongora <rsgmon@gmail.com>
2017-03-14 16:47:19 -0700

    changed folder

--- tests/test_api_retrieval.py
+++ tests/test_api_retrieval.py
@@ -11,13 +11,10 @@ Tests for `api_retrieval` module.
 
 import unittest
 from api_retrieval.setup_api_parameters import *
-# from test
-from tests.test_data_folder.test_data import test_config
+# from tests.test_data_folder.test_data import test_config
 # from tests.test_data.test_data import test_config, api_config, single_api_config, api_config_with_key_route
-# # from api_retrieval.http_request import HTTPRequestHandler
+from api_retrieval.http_request import HTTPRequestHandler
 import api_retrieval.config
-#
-#
 class TestApi_retrieval(unittest.TestCase):
 
     def setUp(self):
