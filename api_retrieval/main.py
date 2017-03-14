f0c9fd1cdbab704852f5136c69c8cdad81e09d26
Ryeland Gongora <rsgmon@gmail.com>
2017-03-14 15:29:55 -0700

    ghgh

--- api_retrieval/main.py
+++ api_retrieval/main.py
@@ -4,7 +4,8 @@ import api_retrieval.setup_api_parameters
 import api_retrieval.http_request
 
 if __name__ == "__main__":
-    config_collection = api_retrieval.setup_api_parameters.config_operation()
-    for config in config_collection:
-        http_request = api_retrieval.http_request.HTTPRequestHandler(config)
-        print(http_request.post_request(http_request.config).json())
+    # config_collection = api_retrieval.setup_api_parameters.config_operation()
+    # for config in config_collection:
+    #     http_request = api_retrieval.http_request.HTTPRequestHandler(config)
+    #     print(http_request.post_request(http_request.config).json())
+    pass
