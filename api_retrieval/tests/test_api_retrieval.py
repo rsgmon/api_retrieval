8391bf40921e11ce613a224949130817fccc5ebc
Ryeland Gongora <rsgmon@gmail.com>
2017-03-14 22:02:01 -0700

    moved directories now work in travis

--- api_retrieval/tests/test_api_retrieval.py
+++ api_retrieval/tests/test_api_retrieval.py
@@ -10,9 +10,10 @@ Tests for `api_retrieval` module.
 
 import unittest
 from api_retrieval.setup_api_parameters import *
-# from api_retrieval.tests.test_data_folder.test_data import test_config, api_config, single_api_config, api_config_with_key_route
-# from api_retrieval.http_request import HTTPRequestHandler
+from api_retrieval.tests.test_data_folder.test_data import test_config, api_config, single_api_config, api_config_with_key_route
+from api_retrieval.http_request import HTTPRequestHandler
 import api_retrieval.config
+
 
 class TestApi_retrieval(unittest.TestCase):
 
@@ -30,22 +31,22 @@ class TestApi_retrieval(unittest.TestCase):
         config_collection = read_config_file(None)
         self.assertEqual(config_collection, None)
 
-    # def test_002_create_config_collection(self):
-    #     config_collection = test_config
-    #     apiconfig_collection = create_config_collection(config_collection)
-    #     self.assertTrue(isinstance(apiconfig_collection[0], api_retrieval.config.APIConfig))
-    #
+    def test_002_create_config_collection(self):
+        config_collection = test_config
+        apiconfig_collection = create_config_collection(config_collection)
+        self.assertTrue(isinstance(apiconfig_collection[0], api_retrieval.config.APIConfig))
+
     # def test_003_setup_http_request_object(self):
     #     http_request_handler = HTTPRequestHandler(single_api_config)
     #     self.assertTrue(type(http_request_handler.config) == APIConfig)
     #
-    # def test_004_construct_configuration(self):
-    #     test_route = "http://www.testing.com/my_route"
-    #     test_parameter = {'api_key': '20349587', 'entity': '0984752039578e4r'}
-    #     self.assertEqual(single_api_config.route, test_route)
-    #     self.assertEqual(single_api_config.parameters, test_parameter)
-    #
-    # def test_005_construct_configuration_with_api_key_route(self):
-    #     test_route = "http://www.testing.com/my_route/20349587"
-    #     self.assertEqual(api_config_with_key_route.route, test_route)
+    def test_004_construct_configuration(self):
+        test_route = "http://www.testing.com/my_route"
+        test_parameter = {'api_key': '20349587', 'entity': '0984752039578e4r'}
+        self.assertEqual(single_api_config.route, test_route)
+        self.assertEqual(single_api_config.parameters, test_parameter)
+
+    def test_005_construct_configuration_with_api_key_route(self):
+        test_route = "http://www.testing.com/my_route/20349587"
+        self.assertEqual(api_config_with_key_route.route, test_route)
 
